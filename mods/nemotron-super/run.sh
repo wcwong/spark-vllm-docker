2d2eff191941430305df2094805f12330b258943
Walter Wong <wcwong@gmail.com>
2026-07-18 19:42:52 +0000

    update with more recipes

--- mods/nemotron-super/run.sh
+++ mods/nemotron-super/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
-cd $WORKSPACE_DIR
-wget https://huggingface.co/nvidia/NVIDIA-Nemotron-3-Super-120B-A12B-NVFP4/resolve/main/super_v3_reasoning_parser.py
+echo "copied super_v3_reasoning_parser.py to $WORKSPACE_DIR"
+cp super_v3_reasoning_parser.py $WORKSPACE_DIR
+
